8dfd2cf35d46ddf9376b320d51610a42d2288ce3
Mokhtaria Benaouali <benaoualimokhtaria@hotmail.com>
2021-12-10 17:44:50 +0100

    update code

--- appQuiz.js
+++ appQuiz.js
@@ -7,31 +7,31 @@ const submitQuiz = document.querySelector(".submit");
 const questions = [
   {
     question: "Who invented JavaScript?",
-    answersProposed: {
+    answers: {
       a: "Douglas Crockford",
       b: "Sheryl Sandberg",
       c: "Brendan Eich"
     },
-    answer: "c"
+    correctAnswer: "c"
   },
   {
     question: "Which one of these is a JavaScript package manager?",
-    answersProposed: {
+    answers: {
       a: "Node.js",
       b: "TypeScript",
       c: "npm"
     },
-    answer: "c"
+    correctAnswer: "c"
   },
   {
     question: "Which tool can you use to ensure code quality?",
-    answersProposed: {
+    answers: {
       a: "Angular",
       b: "jQuery",
       c: "RequireJS",
       d: "ESLint"
     },
-    answer: "d"
+    correctAnswer: "d"
   }
 ];
 
@@ -43,36 +43,65 @@ function quiz(questions, quizContainer){
       let answers;
 
      for(let i=0; i<questions.length; i++){
-		
-		// first reset the list of answers
+		// reset the list of answers
 		answers = [];
 
 		// for each available answer to this question...
-		for(letter in questions[i].answers){
+		for(proposition in questions[i].answers){
 
-			// ...add an html radio button
+			// add an html radio button
 			answers.push(
 				'<label>'
-					+ '<input type="radio" name="question'+i+'" value="'+letter+'">'
-					+ letter + ': '
-					+ questions[i].answers[letter]
+					+ '<input type="radio" name="question'+i+'" value="'+proposition+'">'
+					+ proposition + ': '
+					+ questions[i].answers[proposition]
 				+ '</label>'
 			);
 		}
 
-		// add this question and its answers to the output
 		output.push(
 			'<div class="question">' + questions[i].question + '</div>'
 			+ '<div class="answers">' + answers.join('') + '</div>'
 		);
+         
 	}
 
 	// finally combine our output list into one string of html and put it on the page
 	quizContainer.innerHTML = output.join('');
 }
 //function for reslts
-function showResults(){
-    
+function showResults (questions, quizContainer, results){
+	
+	// gather answer containers from our quiz
+	var answerContainers = quizContainer.querySelectorAll('.answers');
+	
+	// keep track of user's answers
+	var userAnswer = '';
+	var numCorrect = 0;
+	
+	// for each question...
+	for(var i=0; i<questions.length; i++){
+
+		// find selected answer
+		userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+		
+		// if answer is correct
+		if(userAnswer===questions[i].correctAnswer){
+			// add to the number of correct answers
+			numCorrect++;
+			
+			// color the answers green
+			answerContainers[i].style.color = 'green';
+		}
+		// if answer is wrong or blank
+		else{
+			// color the answers red
+			answerContainers[i].style.color = 'red';
+		}
+	}
+
+	// show number of correct answers out of total
+	results.innerHTML = numCorrect + ' out of ' + questions.length;
 }
 
 
@@ -82,5 +111,8 @@ function showResults(){
 //appel fonction
 //quiz container
 quiz(questions, quizContainer);
+
 //show result
-submitQuiz.addEventListener('click', showResults);
+submitQuiz.onclick = function(){
+		showResults(questions, quizContainer, results);
+	};
